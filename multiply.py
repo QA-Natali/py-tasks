f15da9311d002c9373c015a93abb645c89073b00
Natali QA <alice.shelestova@gmail.com>
2026-03-13 21:49:17 +0300

    feat multiply 8ky

---
+++ multiply.py
@@ -0,0 +1,5 @@
+# Multiply - 8ky kata  
+# https://www.codewars.com/kata/50654ddff44f800200000004/train/python
+
+def multiply(a, b): # создали функцию multiply с двумя переменными 
+  return a * b      # возвращаем результат умножения
