380ef19c387a8b0b5c686c7730c6eb1fd57566dc
Natali QA <alice.shelestova@gmail.com>
2026-03-15 19:48:52 +0300

    feat greet ky8

---
+++ greet.py
@@ -0,0 +1,6 @@
+# greet - ky8 - kata
+# https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+
+def greet(name):
+    #Good Luck (like you need it)
+    return 'Hello, ' + name +' how are you doing today?'
