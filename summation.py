b39ec100081046a752c2c633e7dd4a32c03de595
Natali QA <alice.shelestova@gmail.com>
2026-03-16 21:42:39 +0300

    feat summation ky8

---
+++ summation.py
@@ -0,0 +1,8 @@
+# summation.py - summation - ky8 - kata
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+
+def summation(num):
+    x=0
+    for i in range(0, num+1):
+        x = x+i
+    return x   
