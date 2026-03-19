b69955ddfcee22a78b79c8c7bb3046a5e4ac95d7
Natali QA <alice.shelestova@gmail.com>
2026-03-19 21:44:16 +0300

    feat is_divisible ky8

---
+++ is_divisible.py
@@ -0,0 +1,8 @@
+# is_divisible - ky8 - kata
+# https://www.codewars.com/kata/5545f109004975ea66000086/train/python
+
+def is_divisible(n,x,y):
+    if (n % x == 0) and (n % y ==0):
+        return True
+    else:
+        return False
